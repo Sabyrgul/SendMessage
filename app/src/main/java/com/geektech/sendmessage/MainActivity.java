9da0345a68f08f7b9d0de624c6d3184dbe7b7f19
Sabyrgul <quaker.ss11@gmail.com>
2022-12-11 00:20:32 +0600

    extra homework

--- app/src/main/java/com/geektech/sendmessage/MainActivity.java
+++ app/src/main/java/com/geektech/sendmessage/MainActivity.java
@@ -15,28 +15,29 @@ public class MainActivity extends AppCompatActivity {
   EditText editText,editText2;
   Button button;
   TextView textView;
-  Integer s=0;
+  Integer number;
+  String s;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textView=findViewById(R.id.text_view);
         editText=findViewById(R.id.edit_text);
         editText2 = findViewById(R.id.edit_text2);
         button=findViewById(R.id.button);
 
         button.setOnClickListener(view -> {
-            textView.setText(editText2.getText().toString());
-            s= Integer.valueOf(textView.getText().toString());
-            for(int i=0;i<s;i++) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_SEND);
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_TEXT, editText.getText().toString());
-                startActivity(intent);
+            s=editText.getText().toString();
+            number= Integer.valueOf(editText2.getText().toString());
+            for(int i=1;i<number;i++)
+            {
+                s+=" "+editText.getText().toString();
             }
+                Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, s);
+            startActivity(intent);
         });
     }
 
